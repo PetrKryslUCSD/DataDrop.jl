ffcb721a5d8ca8918f516895628b2fd38d790193
Petr Krysl <pkrysl@ucsd.edu>
2021-06-19 16:13:48 -0700

    Initial

---
+++ src/DataValet.jl
@@ -0,0 +1,138 @@
+module DataValet
+
+using JSON
+using SparseArrays
+using HDF5
+
+const _sep = "-"
+
+"""
+    getcleanname(name)
+
+Construct a clean file name.
+"""
+function getcleanname(name)
+    cleanname = replace(replace(replace(name,'.'=>'_'),':'=>'_'),' '=>'_')    
+    return cleanname
+end
+
+"""
+    with_extension(filename, ext)
+
+Make sure the file name has an extension.
+
+`ext` can be either with or without the leading dot.
+```
+a = with_extension(s, ".ext") = "something_or_other_1=0_5_5_7.ext"
+a = with_extension(s, "ext") = "something_or_other_1=0_5_5_7.ext"  
+```
+"""
+function with_extension(filename, ext)
+    if ext == ""
+        # Remove the extension completely
+        f, e = splitext(filename)
+        return f 
+    else        
+        # Replace or fix up the extension
+        if ext[1] == '.'
+            ext = ext[2:end]
+        end
+        if match(Regex(".*\\." * ext * "\$"), filename) == nothing
+            filename = filename * "." * ext
+        end
+    end
+    return filename
+end
+
+"""
+    retrieve_json(j)
+
+Retrieve a dictionary from a JSON file.
+"""
+function retrieve_json(j)
+    j = with_extension(j, "json")
+    return open(j, "r") do file
+        JSON.parse(file)
+    end
+end
+
+"""
+    store_json(j, d)
+
+Store a dictionary into a JSON file.
+"""
+function store_json(j, d)
+    j = with_extension(j, "json")
+    open(j, "w") do file
+        JSON.print(file, d, 4)
+    end
+end
+
+const MATRIX_UNKNOWN = -1
+const MATRIX_DENSE = 0
+const MATRIX_SPARSE = 1
+
+"""
+    retrieve_matrix(fname)
+
+Retrieve a matrix.
+"""
+function retrieve_matrix(fname)
+    fname = with_extension(fname, "h5")
+    typ = MATRIX_UNKNOWN
+    f = try
+        h5open(fname, "r") 
+    catch SystemError
+        nothing
+    end
+    typ = read(f, "matrix_type")
+    if typ == MATRIX_DENSE
+        return read(f, "matrix")
+    elseif typ == MATRIX_SPARSE
+        I = read(f, "I")
+        J = read(f, "J")
+        V = read(f, "V")
+        nrows = read(f, "nrows")
+        ncols = read(f, "ncols")
+        return sparse(I, J, V, nrows, ncols)
+    end
+    return nothing
+end
+
+"""
+    store_matrix(fname, matrix)
+
+Store a matrix.
+"""
+function store_matrix(fname, matrix)
+    fname = with_extension(fname, "h5")
+    h5open(fname, "w") do file
+        write(file, "matrix_type", MATRIX_DENSE) 
+        write(file, "matrix", matrix) 
+    end
+end
+
+
+"""
+    store_matrix(fname, matrix)
+
+Store a matrix.
+"""
+function store_matrix(fname, matrix::SparseArrays.SparseMatrixCSC{Float64, Int64})
+    I, J, V = findnz(matrix)
+    fname = with_extension(fname, "h5")
+    h5open(fname, "w") do file
+        write(file, "matrix_type", MATRIX_SPARSE) 
+        write(file, "I", I) 
+        write(file, "J", J) 
+        write(file, "V", V) 
+        write(file, "nrows", size(matrix, 1)) 
+        write(file, "ncols", size(matrix, 2)) 
+    end
+end
+
+
+
+
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,127 @@
+rm("matrices"; force=true, recursive=true)
+rm("something_or_other_1=0_5_5_7.json"; force=true, recursive=true)
+rm("something_or_other_1=0_5_5_7.mytricks.h5"; force=true, recursive=true)
+rm("something_or_other_1=0_5_5_7.h5"; force=true, recursive=true)
+  
+module mt0001
+using Test
+using DataValet: getcleanname, with_extension
+function test()
+    name = "something or other.1=0.5_5:7"
+    s =  getcleanname(name)
+    @test s == "something_or_other_1=0_5_5_7" 
+    a = with_extension(s, ".ext")
+    @test a == "something_or_other_1=0_5_5_7.ext"
+    a = with_extension(s, "ext")
+    @test a == "something_or_other_1=0_5_5_7.ext"
+    true
+end
+end
+using .mt0001
+mt0001.test()
+
+module mt0002
+using Test
+using DataValet: getcleanname, with_extension
+using DataValet: store_json, retrieve_json
+function test()
+    name = "something or other.1=0.5_5:7"
+    ext = ".json"
+    s =  getcleanname(name)
+    @test s == "something_or_other_1=0_5_5_7" 
+    a = with_extension(s, ext)
+    d = Dict("name" => a, "b" => 1, "c" => [3, 1, 3])
+    store_json(a, d)
+    d1 = retrieve_json(a)
+    @test d == d1
+    true
+end
+end
+using .mt0002
+mt0002.test()
+
+module mt0003
+using Test
+using DataValet: getcleanname, with_extension
+using DataValet: store_matrix, retrieve_matrix
+function test()
+    name = "something.or.other.1=0.5_5:7"
+    ext = ""
+    s =  getcleanname(name)
+    @test s == "something_or_other_1=0_5_5_7"
+    a = with_extension(s, ext)
+    d = rand(3, 2)
+    store_matrix(a, d)
+    d1 = retrieve_matrix(a)
+    @test d == d1
+    true
+end
+end
+using .mt0003
+mt0003.test()
+
+module mt0004
+using Test
+using DataValet: getcleanname, with_extension
+using DataValet: store_matrix, retrieve_matrix
+function test()
+    p = "matrices"
+    mkpath(p)
+    name = joinpath(p, "matrix d")
+    ext = ""
+    s = with_extension(getcleanname(name), ext)
+    d = rand(3, 2)
+    store_matrix(s, d)
+    d1 = retrieve_matrix(s)
+    @test d == d1
+    true
+end
+end
+using .mt0004
+mt0004.test()
+
+
+module mt0005
+using Test
+using DataValet: getcleanname, with_extension
+using DataValet: store_matrix, retrieve_matrix
+function test()
+    p = "matrices"
+    mkpath(p)
+    ext = ""
+    for t in [Int, Float32, Float64, Complex{Float64}]
+        name = joinpath(p, "matrix d " * string(t))
+        s = with_extension(getcleanname(name), ext)
+        d = rand(t, 3, 2)
+        store_matrix(s, d)
+        @test isfile(s * ".h5") == true
+        d1 = retrieve_matrix(s)
+        @test d == d1
+    end
+    true
+end
+end
+using .mt0005
+mt0005.test()
+
+
+module mt0006
+using Test
+using SparseArrays
+using DataValet: getcleanname, with_extension
+using DataValet: store_matrix, retrieve_matrix
+function test()
+    p = "matrices"
+    mkpath(p)
+    name = joinpath(p, "matrix d")
+    ext = ""
+    s = with_extension(getcleanname(name), ext)
+    d = sprand(Float64, 4, 3, 0.75)
+    store_matrix(s, d)
+    d1 = retrieve_matrix(s)
+    @test d == d1
+    true
+end
+end
+using .mt0006
+mt0006.test()
